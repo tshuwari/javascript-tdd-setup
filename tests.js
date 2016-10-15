a71cdd1fcd0d3249c6679f997ca28e10805e9c65
Uloaku <uloshuwari@gmail.com>
2016-10-15 23:17:03 +0100

    Updated tests cases

--- tests.js
+++ tests.js
@@ -3,22 +3,42 @@
 var chai = require('chai');
 var assert = chai.assert;
 
-var lib = require('./lib/library.js');
+var lib = require('./lib/primeSum.js');
+describe("Test should check if a number is prime", function() {
+ it("should give false, for input 9", function() {
+    assert(lib.isPrime(9) == false);
+  });
+  });
+  describe("Test that the sum of the primes are computed properly", function() {
+  it("should give 10, for input 5", function() {
+    assert(lib.sumOfPrimes(5) == 10);
+  });
+  it("should give 17, for input 7", function() {
+    assert(lib.sumOfPrimes(7) == 17);
+  });
+  it("should give 28, for input 11", function() {
+    assert(lib.sumOfPrimes(11) == 28);
+  });
+  it("should give 41, for input 13", function() {
+    assert(lib.sumOfPrimes(13) == 41);
+  });
+  it("should give 58, for input 17", function() {
+    assert(lib.sumOfPrimes(17) == 58);
+  });
+  it("should give 77, for input 19", function() {
+    assert(lib.sumOfPrimes(19) == 77);
+  });
+  it("should give 100, for input 23", function() {
+    assert(lib.sumOfPrimes(23) == 100);
+  }); 
+  });
 
-describe("Test that constants are computed properly", function() {
-  it("should give 10, 4 for constants 2, 5 as a and n respectively", function() {
-    assert(
-      lib.compareCoefficients(lib.quadraticDerivative(2, 5), { a: 10, n: 4 })
-    );
+  describe("Test should check for valid inputs", function() {
+    it("should return invalid input for numbers less than 0", function(){
+    assert(lib.sumOfPrimes(-1) == "Invalid input");
   });
-  it("should give 2, 1 for constants 1, 2 as a and n respectively", function() {
-    assert(
-      lib.compareCoefficients(lib.quadraticDerivative(1, 2), { a: 2, n: 1 })
-    );
-  });
-  it("should give 8, 1 for constants 4, 2 as a and n respectively", function() {
-    assert(
-      lib.compareCoefficients(lib.quadraticDerivative(4, 2), { a: 8, n: 1 })
-    );
-  });
+  it("should return invalid input for non-integers", function() {
+    assert(lib.sumOfPrimes("a") === "Invalid input");
+  }); 
 });
+
